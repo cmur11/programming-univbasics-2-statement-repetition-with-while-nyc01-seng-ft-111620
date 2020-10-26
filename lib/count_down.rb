bd31f6eabe64acf94ac585f5acd252bc153fdd54
Conor Murnane <conormurnane11@gmail.com>
2020-10-26 21:54:28 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -6,3 +6,12 @@ count = 12
   
 #   count /=2
 # end  
+magic_exit_number = 7
+count = 0
+
+while count < 10 do
+  break if count == magic_exit_number
+  puts "I am the #{count}, I love to count!"
+
+  count += 1
+end
