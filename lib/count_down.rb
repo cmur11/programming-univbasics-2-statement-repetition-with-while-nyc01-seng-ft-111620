26efa3eba4072d3ed1a1ba0f214f840ebb667d82
Conor Murnane <conormurnane11@gmail.com>
2020-10-26 21:37:59 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,4 +1,8 @@
 # Write your code here
-while true do
-  puts "say this forever..."
-end
+count = 0 
+
+while count < 3 do
+  puts "I am the #{count}, I love to count!"
+  
+  count = count + 1
+end  
