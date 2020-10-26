c50ec1b89ea60b7fe15311a732864288d5d69429
Conor Murnane <conormurnane11@gmail.com>
2020-10-26 21:59:57 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,5 +1,5 @@
 # Write your code here
-count = 12 
+# count = 12 
 
 # while count >= 3 do
 #   puts "I am the #{count}, I love to count!"
@@ -7,11 +7,22 @@ count = 12
 #   count /=2
 # end  
 
-magic_exit_number = 7
-count = 0
+# magic_exit_number = 7
+# count = 0
 
-while count < 10 && count != magic_exit_number do
-  puts "I am the #{count}, I love to count!"
+# while count < 10 && count != magic_exit_number do
+#   puts "I am the #{count}, I love to count!"
 
-  count += 1
+#   count += 1
+# end
+
+count = 10
+
+while count =<1 do
+  puts count
+  
+else 
+  puts "Happy New Year!"
+  
+  count -=1
 end
